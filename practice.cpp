7000383f931ab833c45550f81b7cc6c225ffe0ef
Shoyondeb <shoyondeb18246@gmail.com>
2025-02-06 14:16:19 +0600

    Fourth to local

---
+++ practice.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>vec;
+        for(int i=0; i<n; i++){
+            int x;
+            cin>>x;
+            vec.push_back(x);
+        }
+        for(int i=0; i<n; i++){
+            cout<<vec[i]<<" ";
+        }
+    }
+    return 0;
+}
